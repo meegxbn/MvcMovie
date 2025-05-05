bffe9b7e4ef1a97bfc301689c52310f44c9862df
Никита Суворов <meeg725@gmail.com>
2025-05-05 15:50:52 +0400

    HelloWorldController

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome()
+        {
+            return "This is my default action...";
+        }
+    }
+}
